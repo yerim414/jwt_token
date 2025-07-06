1fdd0e225552d4276374988370c906dbc9805e3f
yerim <chung2134@gmail.com>
2025-07-06 18:07:29 +0900

    [Add] jwt 핸들러 생성

---
+++ jwt_handler.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+import os
+import datetime
+import jwt
+
+from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+
+
+def create_toke(data: dict, exp_time: int = 30):
+    to_encode = data.copy()
+    expire = datetime.datetime.utcnow() + datetime.timedelta(minutes=exp_time)
+    to_encode.update({"exp": expire})
+    token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return token
+
+
+def decode_token(token: str):
+    try:
+        decoded = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return decoded
+    except ExpiredSignatureError:
+        return {"error": "Token has expired"}
+    except InvalidTokenError:
+        return {"error": "Invalid token"}
