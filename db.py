4c85cc4d258129f8fd60bcd51963e9f0bbbcb64d
yerim <chung2134@gmail.com>
2025-07-06 18:40:38 +0900

    [Fix] 길이 조절 비밀번호 인코딩

--- db.py
+++ db.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
+import urllib.parse
 
 load_dotenv()
 
@@ -12,9 +13,10 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{urllib.parse.quote_plus(DB_PASSWORD)}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+print(DATABASE_URL)
 engine = create_engine(DATABASE_URL, echo=True)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- models/user_model.py
+++ models/user_model.py
@@ -6,6 +6,6 @@ class User(Base):
 
     SEQ = Column(Integer, primary_key=True, index=True)
     ID = Column(String(50), unique=True, nullable=False)
-    PWD = Column(String(50), nullable=False)
+    PWD = Column(String(100), nullable=False)
     NAME = Column(String(50), nullable=False)
     DELETED_AT = Column(DateTime, nullable=True)
