a3d82e2ce0258d1e601c118bd806b86819d3a88f
yerim <chung2134@gmail.com>
2025-08-16 22:32:38 +0900

    [Refactor] 예외처리 한곳에서 처리하도록 수정

--- jwt_handler.py
+++ jwt_handler.py
@@ -28,28 +28,26 @@ def decode_token(token: str):
     try:
         decoded = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         return decoded
-    except ExpiredSignatureError:
-        return {"error": "Token has expired"}
-    except InvalidTokenError:
-        return {"error": "Invalid token"}
-    
-def auth_check(credentials: HTTPAuthorizationCredentials = Depends(security)):
-    if credentials is None:
-        raise HTTPException(status_code=401, detail="권한이 없습니다. 인증 토큰을 제공하세요.")
-
-    token = credentials.credentials
-    try:
-        payload = decode_token(token)
-        user_id = payload.get("user_id")
-        db = SessionLocal()
-        user = db.query(User).filter(User.SEQ == user_id).first()
-        db.close()
-
-        if user is None:
-            raise HTTPException(status_code=401, detail="유효하지 않은 토큰입니다.")
-        return user
-
     except jwt.ExpiredSignatureError:
         raise HTTPException(status_code=401, detail="토큰이 만료되었습니다.")
     except Exception:
         raise HTTPException(status_code=401, detail="유효하지 않은 토큰입니다.")
+
+
+def auth_check(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    if credentials is None:
+        raise HTTPException(
+            status_code=401, detail="권한이 없습니다. 인증 토큰을 제공하세요."
+        )
+
+    token = credentials.credentials
+
+    payload = decode_token(token)
+    user_id = payload.get("user_id")
+    db = SessionLocal()
+    user = db.query(User).filter(User.SEQ == user_id).first()
+    db.close()
+
+    if user is None:
+        raise HTTPException(status_code=401, detail="유효하지 않은 토큰입니다.")
+    return user
